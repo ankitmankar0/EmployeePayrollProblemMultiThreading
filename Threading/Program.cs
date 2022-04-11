bed841e5d6d4c93495f39f377d8f332ee657af3b
ankitmankar0 <ankitmankar012@gmail.com>
2022-04-11 13:16:38 +0530

    Added Emp Payroll Threads

---
+++ Threading/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Payroll System Using Threads.");
+
+        }
+    }
+}
+
